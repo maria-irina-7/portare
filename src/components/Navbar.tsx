81f5d0eca87c759fe12537fba3f7a4b32735656d
Maria Irina <maria.irina.niculescu@outlook.com>
2026-06-28 17:19:44 +0300

    Use temporarly MobileIcon in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "../styles/Navbar.css";
+import MobileIcon from "../assets/MobileIcon";
 
 interface NavbarProps {
-  items: string[];
+  items: React.ReactNode[];
 }
 
 const Navbar = ({ items }: NavbarProps) => {
   return (
     <div className="nav">
-      <span className="nav__title">Compară prețurile abonamentelor de</span>
+      <span className="nav__title">Compară prețurile abonamentelor de:</span>
       <ul className="nav__menu">
         {items?.map((item, index) => (
           <li key={index} className="nav__menu_item active">
+            <MobileIcon />
             {item}
           </li>
         ))}
