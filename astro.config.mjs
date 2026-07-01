9840f40f49a2d9727154898815647ac3a49f1773
Maria Irina <maria.irina.niculescu@outlook.com>
2026-07-01 22:37:06 +0300

    Added mdx dependency to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
   site: "https://maria-irina-7.github.io",
   base: "/portare",
 });
