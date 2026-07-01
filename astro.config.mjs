9a8d3a3ee21961702a99e5b68ef1d904b9ace72f
Maria Irina <maria.irina.niculescu@outlook.com>
2026-07-02 01:11:04 +0300

    Remove base property

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,5 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
   integrations: [react(), mdx()],
   site: "https://maria-irina-7.github.io",
-  base: "/portare",
+  // base: "/portare",
 });
